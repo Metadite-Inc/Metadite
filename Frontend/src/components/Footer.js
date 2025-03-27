20caf501adeeeb3fd7805c802c94e4433edf1f64
the-1Riddle <154701770+the-1Riddle@users.noreply.github.com>
2025-03-27 23:58:36 +0300

    Delete Frontend directory

--- Frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer style={{ backgroundColor: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
-    <p>© 2025 Metadite. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
