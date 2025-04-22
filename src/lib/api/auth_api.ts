383997fa69713236e7e6ef6126bee82d6ebe4cd8
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2025-04-22 19:59:19 +0300

    Update auth_api.ts

--- src/lib/api/auth_api.ts
+++ src/lib/api/auth_api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000'; // Update this with your actual backend URL
+const API_BASE_URL = import.meta.env.BACKEND_API_URL;
 
 interface LoginRequest {
   email: string;
