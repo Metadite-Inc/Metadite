c2ed2a672341b19ddcff31c7323c3f0feea03d68
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2025-04-28 07:20:19 -0700

    From phase: Update landing page and regions. (#76)
    * add dolls, display them, done with
    * Phase: prepare phase to be merged with main (#75)
    * slideshow
    
    ---------
    
    Co-authored-by: the-1Riddle <154701770+the-1Riddle@users.noreply.github.com>
    Co-authored-by: eric karanja <erickaranja54@gmail.com>

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -446,7 +446,7 @@ const Checkout = () => {
                       )}
                     </div>
                     
-                    <div 
+                    {/*<div 
                       onClick={() => setPaymentMethod('stripe')}
                       className={`border rounded-lg p-4 cursor-pointer transition-colors ${
                         paymentMethod === 'stripe' 
@@ -456,7 +456,7 @@ const Checkout = () => {
                             : 'border-gray-200 hover:border-gray-300'
                       }`}
                     >
-                      <div className="flex items-center">
+                      {/*<div className="flex items-center">
                         <div className={`w-5 h-5 rounded-full border mr-3 flex items-center justify-center ${
                           paymentMethod === 'stripe' 
                             ? 'border-metadite-primary' 
@@ -467,11 +467,11 @@ const Checkout = () => {
                           {paymentMethod === 'stripe' && (
                             <div className="w-3 h-3 rounded-full bg-metadite-primary"></div>
                           )}
-                        </div>
-                        <span className={`font-medium ${theme === 'dark' ? 'text-white' : ''}`}>Stripe</span>
-                        <span className={`ml-2 text-xs ${theme === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>(International payments)</span>
+                       </div>
+                       <span className={`font-medium ${theme === 'dark' ? 'text-white' : ''}`}>Stripe</span>
+                       <span className={`ml-2 text-xs ${theme === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>(International payments)</span>
                       </div>
-                    </div>
+                    </div>*/}
 
                     <div 
                       onClick={() => setPaymentMethod('crypto')}
