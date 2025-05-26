72bd1467103ecee49ad20a119b5b897c46935e1b
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2025-05-25 18:03:11 -0700

    Revert "No ttt: one size (#153)" (#154)
    This reverts commit a88b4228b0fb21ec761bf138de8b515bb86ffe05.

--- src/pages/model/components/ModelGallery.jsx
+++ src/pages/model/components/ModelGallery.jsx
@@ -5,19 +5,19 @@ const ModelGallery = ({ images, mainImage, setMainImage }) => {
   return (
     <div>
       {/* Large screens */}
-      <div className="hidden sm:flex rounded-lg overflow-hidden bg-white mb-4 w-full h-[444px] items-center justify-center">
+      <div className="hidden sm:flex rounded-lg overflow-hidden bg-white mb-4 aspect-[3/4.5] h-100 w-96 items-center justify-center">
         <img 
           src={mainImage} 
           alt="Model" 
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain"
         />
       </div>
       {/* Small screens */}
-      <div className="flex sm:hidden rounded-lg overflow-hidden bg-white mb-4 w-full h-[444px] items-center justify-center">
+      <div className="flex sm:hidden rounded-lg overflow-hidden bg-white mb-4 w-100 h-100 aspect-[3/4.5] items-center justify-center">
         <img 
           src={mainImage} 
           alt="Model" 
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain"
         />
       </div>
       
